c77b4373d266049957b98678fa5e0ca8a1b1c87f
cike-567 <68531942+cike-567@users.noreply.github.com>
2024-06-10 15:39:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,21 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+#  修改拉取的 smartdns 源代码版本。
+sed -i "s/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=07c13827bb523519a638214ed7ad76180f71a40a/" feeds/packages/net/smartdns/Makefile
+
+#  建立 2333 目录用于存放杂项。
+mkdir 2333
+
+#  将工作目录从 Openwrt 源代码目录切换到 2333 目录。
+cd 2333
+
+#  拉取 luci-app-smartdns 的源码。
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git
+
+#  将工作目录从 2333 目录切换到 Openwrt 源代码目录。
+cd ..
+
+#  删除自带的 luci-app-smartdns 的源码
+rm -rf feeds/luci/applications/luci-app-smartdns/*
